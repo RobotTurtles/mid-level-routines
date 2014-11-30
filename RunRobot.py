65bc36d5c9b31411ad808657b121be729a9ddf5c
Alex Gutierrez <alex@2d12.net>
2014-11-29 17:55:30 -0800

    Using Curses Library

--- RunRobot.py
+++ RunRobot.py
@@ -1,17 +1,23 @@
 # Simple Robot Program
 
-from msvcrt import getch
+import curses
 from Movement import Movement
+
+win = curses.initscr()
+win.nodelay(1)
+curses.noecho()
 
 m = Movement()
 distanceCM = 2
 distanceDegrees = 2
 
 while True:
-    key = ord(getch())
-    if key == 27: #ESC
-        break
-    elif key == 224: #Special keys (arrows, f keys, ins, del, etc.)
+	c = win.getch()
+	key = ord(c)
+	
+	if key == 27: #ESC
+		break
+	elif key == 224: #Special keys (arrows, f keys, ins, del, etc.)
 		key = ord(getch())
 		
 		if key == 80: #Down arrow
