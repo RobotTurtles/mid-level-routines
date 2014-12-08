42f4b254c289585e4a48415fc745151ece688975
Alex Gutierrez <alex@2d12.net>
2014-12-07 18:23:02 -0800

    slight mod for turning

--- TurnToFace.py
+++ TurnToFace.py
@@ -10,7 +10,7 @@ import time
 
 m = Movement()
 f = FaceRecognition()
-threshold = 10
+threshold = 5
 
 while(True):
 	faceLocation = f.FindFace()
@@ -18,7 +18,15 @@ while(True):
 	if(faceLocation[0] != 0 and faceLocation[1] != 0):
 		delta = 240 - faceLocation[0]
 		
-		if(abs(delta) > threshold):
-			m.turnDegrees(-delta)
+		print "delta:" + str(delta)
+		
+		# turn left
+		if(delta > threshold):
+			m.turnDegrees(delta)
 			
+		#turn right
+		if(delta < -threshold):
+			m.turnDegrees(delta)
+		
+		
 	time.sleep(2)
