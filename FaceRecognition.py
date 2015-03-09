7881fecf58a2fe8a3b975f8e055cb3ec7caa1307
Alex Gutierrez <alex@2d12.net>
2015-03-08 20:43:39 -0700

    Fixing indents

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -24,20 +24,20 @@ class FaceRecognition:
 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 		
 		faces = self.frontalface.detectMultiScale(gray, 1.3, 5)
-			for (x,y,w,h) in faces:
-				cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-				self.Cface = [(w/2+x),(h/2+y),w]
+		for (x,y,w,h) in faces:
+			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+			self.Cface = [(w/2+x),(h/2+y),w]
 				
-				if(x != 0 and y != 0):
-					filename = capturePath + '/' + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S.jpg")
-					self.logger.info(filename)
-					cv2.imwrite(filename, img)
-			
-			if(self.Cface[2] == 0):
-				filename = missedPath + '/' + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S.jpg")	
+			if(x != 0 and y != 0):
+				filename = capturePath + '/' + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S.jpg")
 				self.logger.info(filename)
 				cv2.imwrite(filename, img)
 			
-			self.logger.info(str(self.Cface[0]) + "," + str(self.Cface[1]) + ",width:"+ str(self.Cface[2]))
-			self.webcam.release()	
-			return self.Cface
+		if(self.Cface[2] == 0):
+			filename = missedPath + '/' + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S.jpg")	
+			self.logger.info(filename)
+			cv2.imwrite(filename, img)
+			
+		self.logger.info(str(self.Cface[0]) + "," + str(self.Cface[1]) + ",width:"+ str(self.Cface[2]))
+		self.webcam.release()	
+		return self.Cface
