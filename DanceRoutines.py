7bf38a78fd5747830cc2e6bfc33de4db7899d992
Alex Gutierrez <alex@2d12.net>
2015-12-17 12:23:04 -0800

    Added QR Codes, ability to "dance"

--- DanceRoutines.py
+++ DanceRoutines.py
@@ -1,4 +1,5 @@
 from Movement import Movement
+import shlex
 
 class DanceRoutines:
     '''
@@ -14,6 +15,17 @@ class DanceRoutines:
         self.nodDistance = 30
         self.shakeDistance = 30
         self.logger.info("Initialized Dance Routines")
+
+
+    def dances(self, com):
+        return {
+            'rightHokiepokie':self.rightHokiePokie,
+            'leftHokiepokie':self.leftHokiePokie,
+            'nod':self.nod,
+            'shakeNo':self.shakeNo,
+            'spinRight':self.spinRight,
+            'spinLeft':self.spinLeft,
+        }[com]
 
     def spinRight(self):
         self.logger.info("DanceRoutines: Spin Right")
@@ -72,3 +84,38 @@ class DanceRoutines:
         # That is what it is all about :)
         self.m.moveCM(moveDistance)
         self.m.moveCM(-moveDistance)
+
+
+
+    def executeDance(self, danceMoves):
+        '''
+        Must be a series of commands in the form of <motion> <value>
+        Example Commands that can be used are:
+            moveCM 20
+            moveCM -20
+            turnDegrees 20
+            turnDegrees -20
+            left 20
+            right 20
+        :param danceMoves:
+        :return:
+        '''
+        my_splitter = shlex.shlex(danceMoves)
+        my_splitter.whitespace += ','
+        my_splitter.whitespace_split = True
+
+        print list(my_splitter)
+        self.logger.info(list(my_splitter));
+
+
+        for command in my_splitter:
+            print str(command)
+            details = command.split()
+
+            # Execute Command
+            self.dances(details[0])()
+
+
+
+
+
