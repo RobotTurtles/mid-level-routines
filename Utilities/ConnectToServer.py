be6e7f489addc74fbfb7f3e3991310596696671e
aerophilic <alex@2d12.net>
2016-02-27 23:38:02 -0800

    Updating wget

--- Utilities/ConnectToServer.py
+++ Utilities/ConnectToServer.py
@@ -8,12 +8,16 @@
 import subprocess
 import socket
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 0))  # connecting to a UDP address doesn't send packets
+local_ip_address = s.getsockname()[0]
+
 baseUrl = 'https://secret-brushlands-1127.herokuapp.com'
 path = '/turtleinfo?'
 turtle_name = socket.gethostname()
-ip_address = socket.gethostbyname(socket.gethostname())
+ip_address = local_ip_address
 turtle_id = '3'
 
 sendUrl = baseUrl + path + 'turtle_name='+turtle_name + '&ip_address=' + str(ip_address)+ '&turtle_id=' + turtle_id
 
-response = subprocess.check_output(['/usr/bin/wget',sendUrl]).split('\n')
+response = subprocess.check_output(['/usr/bin/wget', '--quiet','--spider',sendUrl]).split('\n')
