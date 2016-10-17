97f1291e0e7da3837f9cf441a159f0c1272220e1
aerophilic <alex@2d12.net>
2016-10-16 18:16:06 -0600

    Initial Web Connection

---
+++ www/robot.py
@@ -0,0 +1,33 @@
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
+
+print "Content-type: text/html"
+print
+
+print """
+<html>
+<head><title>Turtle Response</title></head>
+<body>
+Turtle Saw: 
+"""
+form = cgi.FieldStorage()
+
+r_cmd = form.getvalue("r_cmd", "{no message}")
+r_arg = form.getvalue("r_arg", "{no arg}")
+
+print """
+   <p>Previous message: %s, args: %s</p>
+
+   <p>SendCommand:
+  <form method="post" action="robot.py">    
+	<p>message: <input type="text" name="r_cmd"/></p>
+	<p>argument: <input type="text" name="r_arg"/></p>
+	<input type="submit" name="Update">
+  </form>
+
+</body>
+
+</html>
+""" % (cgi.escape(r_cmd), cgi.escape(r_arg))
+
+
